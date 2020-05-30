967ea0eefbe67c5ef79de8748493ebb145f91d02
SunRiseGG <SunRiseGC4@gmail.com>
2020-05-30 21:08:28 +0300

    Intergation test fix

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -8,7 +8,12 @@ import (
 	"time"
 )
 
-const baseAddress = "http://localhost:8090"
+const baseAddress = "http://balancer:8090"
+
+var servers = make(map[string]int)
+servers["server1:8080"] = 0
+servers["server2:8080"] = 0
+servers["server3:8080"] = 0
 
 var client1 = http.Client{
 	Timeout: 10 * time.Second,
@@ -20,6 +25,7 @@ var client3 = http.Client{
 	Timeout: 10 * time.Second,
 }
 
+
 func Test(t *testing.T) { TestingT(t) }
 
 type TestSuite struct{}
@@ -28,15 +34,17 @@ var _ = Suite(&TestSuite{})
 
 func (s * TestSuite) TestBalancer(c *C) {
 	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	count  := 0
-  count1 := 0
-  count2 := 0
-  count3 := 0
+	counter  := 0
 
   for range time.Tick(1 * time.Second) {
 	  resp1, err1 := client1.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 	  resp2, err2 := client2.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 	  resp3, err3 := client3.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		responsesPool = []string{
+			resp1.Header.Get("lb-from"),
+			resp2.Header.Get("lb-from"),
+			resp3.Header.Get("lb-from"),
+		}
 	  if err1 != nil {
 		  c.Error(err1)
 	  }
@@ -46,41 +54,18 @@ func (s * TestSuite) TestBalancer(c *C) {
 	  if err3 != nil {
 		  c.Error(err3)
 	  }
-	  switch resp1.Header.Get("lb-from") {
-	    case "server1:8080":
-			  count1++
-		  case "server2:8080":
-		  	count2++
-		  case "server3:8080":
-			  count3++
-	  }
-		switch resp2.Header.Get("lb-from") {
-	    case "server1:8080":
-			  count1++
-		  case "server2:8080":
-		  	count2++
-		  case "server3:8080":
-			  count3++
-	  }
-		switch resp3.Header.Get("lb-from") {
-	    case "server1:8080":
-			  count1++
-		  case "server2:8080":
-		  	count2++
-		  case "server3:8080":
-			  count3++
-	  }
-	  c.Log("response from", resp1.Header.Get("lb-from"))
-	  c.Log("response from", resp2.Header.Get("lb-from"))
-	  c.Log("response from", resp3.Header.Get("lb-from"))
-		count++
-		if count == 20 {
+		for responseHeader := range responsesPool {
+			servers[responseHeader]++
+			c.Log("response from", responseHeader)
+		}
+		counter++
+		if counter == 20 {
 			break
 		}
 	}
-	c.Log("Total responses from", count1)
-	c.Log("Total responses from", count2)
-	c.Log("Total responses from", count3)
+	for _, value := range servers {
+		c.Log("Total responses from", key, value)
+	}
 }
 
 
